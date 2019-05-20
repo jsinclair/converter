597e0898a600be229e4e1c93875d3f1fee0b8446
James Sinclair <james@overtake.co.za>
2019-05-20 11:01:05 +0200

    Updated the runner, removed some files that should have been ignored.

--- converter-api/target/dependency/BOOT-INF/classes/data-postgres.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users(username, password) VALUES('user', 'pass');
-INSERT INTO users(username, password) VALUES('james', 'password');
-INSERT INTO users(username, password) VALUES('ruben', 'password');
-INSERT INTO users(username, password) VALUES('philip', 'password');
